ff077d21b40309d986fbacc55757d44f47d22566
Miguel <mikevelasquez02@users.noreply.github.com>
2016-03-16 22:18:37 -0500

    Starting Point

---
+++ loginPage.php
@@ -0,0 +1,27 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>CSCE Group 15's project</title>
+
+	<script type="text/javascript">
+	function login_Validation() {
+		
+			// if id and password not matching from database return false 
+			// else return true
+			return true;
+		
+	</script>
+<head>
+		<title>	CSCE Group 15's project</title>
+	</head>
+	<h1>Welcome to CSCE Group 15's project page</h1>
+	<body align="center" style="background-color:#247">
+
+
+	<form id = "loginform" action="mainPage.php" method="post" onsubmit="return login_Validation()" >
+		User ID  : <input style="margin-left:16px" type="text" name="user_log" id= "user_log" value=""><br><br>
+		Password : <input style="margin-left:2px" type="text" name="password_log" id= "password_log" value=""><br><br>
+		<input type="submit" value="Login">
+	</form>
+	
+	</body>
+</html>
