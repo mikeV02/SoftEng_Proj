59be14026ff23d78de039a81b37d7157da79a05c
Miguel <mikevelasquez02@users.noreply.github.com>
2016-03-17 03:32:38 -0500

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -48,7 +48,7 @@
 		}
 	?>
 
-	<form id = "loginform" action="mainPage.php" method="post" onsubmit="return login_Validation()" >
+	<form id = "loginform" <!--action="mainPage.php"--> method="post" onsubmit="return login_Validation()" >
 		User ID  : <input style="margin-left:16px" type="text" name="user_log" id= "user_log" value=""><br><br>
 		Password : <input style="margin-left:2px" type="text" name="password_log" id= "password_log" value=""><br><br>
 		<input type="submit" value="Login">
