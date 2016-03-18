faa4649f5845f1392d55321125f8536fa413f8c0
frmonast <fabianm.riveros@gmail.com>
2016-03-18 00:18:04 -0500

    Update newAccount.php

--- newAccount.php
+++ newAccount.php
@@ -58,10 +58,6 @@ function account_Validation() {
 	$con = mysql_connect("localhost", $username, $password);
 	$db_found = mysql_select_db($db, $con);
 	
-	//echo $user_nam;
-	//echo $uemail;
-	//echo $passreg;
-	
 	
 	if($db_found){
 		echo "DB Found";
@@ -70,9 +66,8 @@ function account_Validation() {
 		//	if($passreg == $passcheck){
 				//$con = mysql_connect(0, 'localhost', $username, $password);
 				//$db_found = mysql_select_db($db, $con) or die ("Unable to connect to specific database.");
-	mysql_query("INSERT INTO `softeng`.`users` (`ID`, `user_login`, `user_pass`, `user_email`, `user_registered`, `user_status`, `display_name`) VALUES(2, $user_nam, $passreg, $uemail, NULL, NULL, NULL)");
-	
-				//mysql_query("INSERT INTO users VALUES(2, $user_nam, $passreg, $uemail, NULL, NULL, NULL)");
+				mysql_query("INSERT INTO users (ID, user_login, user_pass, user_email, user_registered, user_status, display_name) 
+					VALUES(2, $user_nam, $passreg, $uemail, NULL, NULL, NULL)");
 				echo "Account Created";
 				mysql_close($con);
 				$redirect=sprintf("Location: http://softeng.mikedlv.com/mainPage.php?userID=%s"
