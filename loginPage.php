9b4997c06ea19dc53199286ae55a5f8e3299ecba
Miguel <mikevelasquez02@users.noreply.github.com>
2016-03-17 21:19:07 -0500

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -73,7 +73,7 @@
 		and we do not reaveal sensitive data
 		
 	action="mainPage.php"-->
-	<form id = "loginform" action="mainPage.php" method="post" onsubmit="return login_Validation()" >
+	<form id = "loginform" action="loginPage.php" method="post" onsubmit="return login_Validation()" >
 		User ID  : <input style="margin-left:16px" type="text" name="user_log" id="user_log" value=""><br><br>
 		Password : <input style="margin-left:2px" type="text" name="password_log" id= "password_log" value=""><br><br>
 		<input type="submit" value="Login">
