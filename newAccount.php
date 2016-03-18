2286054a70ae5e6e2906052627bd1961f1d58db8
frmonast <fabianm.riveros@gmail.com>
2016-03-18 01:11:43 -0500

    Update newAccount.php

--- newAccount.php
+++ newAccount.php
@@ -59,12 +59,11 @@ function account_Validation() {
 	//$db_found = mysql_select_db($db, $con);
 	$sql= "INSERT INTO users (ID, user_login, user_pass, user_email, user_registered, user_status, display_name) 
 		VALUES(NULL, '$user_nam', '$passreg', '$uemail', NULL, 0, '$user_nam')";
-	//if(mysqli_query($con, $sql)){
+	if(mysqli_query($con, $sql)){
 		if(($user_nam=="")||($passreg=="")||($passcheck=="")){
 			echo "Inser your credentials";
 		} else{
-			if($passreg == $passcheck)
-				if(mysqli_query($con, $sql)){
+			if($passreg == $passcheck){
 				echo "Account Created";
 				mysqli_close($con);
 				$redirect=sprintf("Location: http://softeng.mikedlv.com/mainPage.php?userID=%s"
@@ -74,9 +73,9 @@ function account_Validation() {
 				echo "Please make sure both passwords are the same.";
 			}
 			}
-		}// else{
-		//	echo "ERROR";
-		//}
+		} else{
+			echo "ERROR";
+		}
 ?>
 
 	<head>
