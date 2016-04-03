7baba5256c3f223c03e31920e56af4276dba8ef5
Miguel <admin@mikedlv.com>
2016-04-02 22:14:48 -0500

    delAutoPull

--- pull.php
+++
@@ -1,9 +0,0 @@
-<?php
-<<<<<<< HEAD
-  $out = shell_exec('sh /home/ubuntu/Desktop/Files/scripts/pull'); 
-  echo "$out";
-=======
-  $output = shell_exec('sh /home/ubuntu/Desktop/Files/scripts/pull');
-  echo "$output";
->>>>>>> ebc2680ea8553c6f073f8ec058b3a20f4c63f23a
-?>
