156fe08192fd09740f1bb74f34280077a38882ad
dacalder <dacalder@uark.edu>
2016-04-04 18:38:22 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -12,8 +12,12 @@ $result = mysqli_query($con, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
+    $videos = array();
+    $i = 0;
     while($row = mysqli_fetch_assoc($result)) {
         echo "url: " . $row['name']. "<br>";
+        $videos[i] = $row['name'];
+        $i = $i +1;
     }
 } else {
     echo "0 results";
