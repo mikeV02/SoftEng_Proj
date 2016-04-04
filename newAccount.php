d21f9455df689ff69505ad71e15a409ccba5aebd
frmonast <fabianm.riveros@gmail.com>
2016-04-03 21:09:56 -0500

    Update newAccount.php

--- newAccount.php
+++ newAccount.php
@@ -62,7 +62,7 @@ function account_Validation() {
 	if(!$con){
 		die('Not connected : '.mysql_error());
 	}
-	if((isset($user_nam)) && (isset($uemail))){
+	if((isset($user_nam)) && (isset($uemail)) && (isset($passreg))){
 		echo "All data Inserted.";
 
 	}
