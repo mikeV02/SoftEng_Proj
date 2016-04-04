4ccd884560525179b56237589d84f9617af84ae3
dacalder <dacalder@uark.edu>
2016-04-04 00:31:17 -0500

    Update uploadvideo.php

--- uploadvideo.php
+++ uploadvideo.php
@@ -28,7 +28,7 @@ if(isset($_POST['upload']))
 	move_uploaded_file($video_tmp_name, "videos/$video_name");
 	//$url="/home/ubuntu/Desktop/Files/";
 	$url = "http://softeng.mikedlv.com/videos/$video_name";
-	$sql = "INSERT INTO videos (name, ulr) VALUE ('$video_name','$url')";
+	$sql = "INSERT INTO videos (name, url) VALUE ('$video_name','$url')";
 	$result = mysqli_query($con, $sql);
 	if(!isset($result)){
 		echo("error database");			
