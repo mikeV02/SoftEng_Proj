a7482dfd0a729a1b4a6437e25d5b75a40d93c6fa
dacalder <dacalder@uark.edu>
2016-04-04 00:11:08 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -9,6 +9,15 @@ if(!$con)
 {
 	die('Not connected' .mysql_error());
 }
+$query = "SELECT text AS txt FROM `videos`";
+$result = mysqli_query($con, $query) or trigger_error("Query Failed! SQL: $query - Error: ". mysqli_error($con), E_USER_ERROR);
+
+if($result) {
+	while($row = mysqli_fetch_assoc($result)) {
+		echo $row['txt'] . '<br>';	
+	}
+}
+mysqli_close($mysqli);
 ?>
 <!doctype html>
 <html>
