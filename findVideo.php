1d3a54747a3721e218b53d8dd22ea77970a2f4b5
dacalder <dacalder@uark.edu>
2016-04-04 00:03:22 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -1,28 +1,4 @@
-<?php
-$db = "softeng";
-$password = "softeng";
-$username = "root";
-$db2 = "videos";
-$conn = mysqli_connect("localhost", $username, $password, $db);
-if(!$conn)
-{
-	die("Connection failed: ". mysqli_connect_error());
-}
 
-$sql = "SELECT url FROM videos";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "Url: " . $row["ulr"]"<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-mysqli_close($conn);
-?>
 <!doctype html>
 <html>
 <head>
@@ -35,20 +11,7 @@ mysqli_close($conn);
 
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
-
-
-
-<?php
-if(isset($_POST['submit']))
-{
-	echo "<br />".$name." has been uploaded";
-}
-
+<p>Find video</p>
 
 ?>
 
