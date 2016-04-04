cf206b5bb9f1737a3d216517f4d4013424761cc1
dacalder <dacalder@uark.edu>
2016-04-03 21:27:39 -0500

    Update uploadvideo.php

--- uploadvideo.php
+++ uploadvideo.php
@@ -3,8 +3,8 @@ $db ="softeng";
 $password = "softeng";
 $username = "root";
 
-$con = $mysql_connect("localhost",$username, $password, $db);
-mysql_select_db($db , $con);
+$con = $mysql_connect("localhost","root","");
+mysql_select_db($db);
 
 if(isset($_POST['submit']))
 {
