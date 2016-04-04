46d656d67f88662ee3bdeda930f3e905902577af
dacalder <dacalder@uark.edu>
2016-04-04 00:28:45 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -6,6 +6,14 @@ $username = "root";
 $db2 = "videos";
 $con = mysqli_connect("localhost", $username, $password, $db) OR die('Could not connect to SQL:'. mysqli_connect_error());
 
+//display videos that are in database
+$sql = "SELECT url FROM videos";
+$result = mysqli_query($con, $sql);
+while($row = mysqli_fetch_assoc($result))
+{
+  echo"{$row['url']};
+}
+
 mysqli_close($con);
 ?>
 <!doctype html>
