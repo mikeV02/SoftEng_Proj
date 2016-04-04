20950ed9206991797c8628d3f33ef38b4a13fc33
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-03 22:18:54 -0500

    Update addDBtst.php

--- addDBtst.php
+++ addDBtst.php
@@ -31,6 +31,10 @@
     if(mysql_query($sql)){
         $message = "Account Created";
 		echo "<script type='text/javascript'>alert('$message');</script>";
+	$redirect=sprintf("Location: http://softeng.mikedlv.com/mainPage.php?userID=%s"
+				, mysql_real_escape_string($user));
+				mysqli_close($link);
+				header($redirect);
         echo "Records added successfully.";
 
     } else{
