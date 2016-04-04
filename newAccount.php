3282ad1502b292f3773d569c0f1235f451ecccfb
frmonast <fabianm.riveros@gmail.com>
2016-04-03 21:26:20 -0500

    Update newAccount.php

--- newAccount.php
+++ newAccount.php
@@ -6,45 +6,13 @@
 <script type="text/javascript">
 
 function account_Validation() {
-/*
-			var email = document.forms["createform"]["email"].value;
-            var pass = document.forms["createform"]["password"].value;
-			if ((document.forms["createform"]["userId"].value) == "") {
-				alert('Password cannot be left blank');
-				return false;
-			}
-			
-			if ((document.forms["createform"]["password"].value) == "") {
-				alert('Password cannot be left blank');
-				return false;
-			}
-			
-			if ((document.forms["createform"]["fname"].value) == "") {
-				alert('First name cannot be left blank');
-				return false;
-			}
-			if ((document.forms["createform"]["lname"].value) == "") {
-				alert('Last name cannot be left blank');
-				return false;
-			}
-
-			if ((document.forms["createform"]["email"].value) == "") {
-				alert('Email cannot be left blank');
-				return false;
-			}
-			if ((email.search("@") < 0)||(email.indexOf(".") < 0)) {
-				alert('Invalid form of email');
-				return false;
-			}
-			
-			return true;
-*/
-
+	//Whatever was here was not doing anything.
 }
 </script>
 
 
 <!-- Added by Fabian Monasterio -->
+<!-- Code below should create new user in the database without any problems. -->
 <?php
 
 	$user_nam = $_POST["uname"];
@@ -80,47 +48,6 @@ function account_Validation() {
 		}
 
 	}
-
-
-
-/*
-	if(($user_nam=="")||($passreg=="")||($passcheck=="")){
-			echo "Insert your credentials";
-	} else{
-		if($passreg == $passcheck){
-			mysqli_query($con, $sql);
-			echo "Account Created";
-			mysqli_close($con);
-			$redirect=sprintf("Location: http://softeng.mikedlv.com/mainPage.php?userID=%s"
-				, mysql_real_escape_string($username));
-			header($redirect);
-		} else{
-			echo "Please make sure both passwords are the same.";
-		}
-	}
-	
-	//$sql= "INSERT INTO users (ID, user_login, user_pass, user_email, user_registered, user_status, display_name) 
-	//	VALUES(NULL, '$user_nam', '$passreg', '$uemail', NULL, 0, '$user_nam')";
-		
-	if(mysqli_query($con, $sql)){
-		if(($user_nam=="")||($passreg=="")||($passcheck=="")){
-			echo "Inser your credentials";
-		} else{
-			if($passreg == $passcheck){
-				//mysqli_query($con, $sql);
-				echo "Account Created";
-				mysqli_close($con);
-				$redirect=sprintf("Location: http://softeng.mikedlv.com/mainPage.php?userID=%s"
-				, mysql_real_escape_string($username));
-				header($redirect);
-			} else{
-				echo "Please make sure both passwords are the same.";
-			}
-		}
-	} else{
-		echo "ERROR";
-	}
-*/
 ?>
 
 	<head>
@@ -147,16 +74,4 @@ function account_Validation() {
 	</form>
 </body>
 </html>
-<!--
-		<p>Please fill out the form below to make a new account.</p>	
-		<form id = "createform" action="mainPage.php" method="post" onsubmit="return account_Validation()" >
-		User ID           : <input  style="margin-left:80px" type="text" name="user" id= "userId" value=""><br>
-		Password          : <input style="margin-left:67px" type="text" name="password" id= "password" value=""><br>
-		Your first Name   : <input style="margin-left:20px" type="text" name="fname" id= "fname" value=""><br>
-		Your last Name    : <input style="margin-left:22px" type="text" name="lname" id="lname" value=""><br>
-		Your email address: <input type="text" name="email" id="email" value=""><br>
-		<br>
-		<input type="submit" value="Create a New Account">
-		</form>
 
--->
