3d7684163847f04b0c09c62a72239b6709eadf56
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-04 03:10:35 -0500

    Create index.php

---
+++ videos/index.php
@@ -0,0 +1,44 @@
+me = $_FILES['file']['name'];
+	$temp = $_FILES['file']['tmp_name'];
+	
+	move_uploaded_file($temp, "uploaded/".$name);
+	$url = "http://softeng.mikedlv.com/videos/uploaded/$name";
+	mysql_query("INSERT INTO 'videos' VALUE ('','$name','$url')");
+	
+}
+
+
+
+?>
+
+<!doctype html>
+<html>
+<head>
+
+<meta charset = "utf-8">
+<title> Video Upload </title>
+</head>
+
+<h1> Upload Video  </h1>
+
+<body>
+
+<form action = "index.php" method="POST" enctype="multipart/form-data>
+	<input type="file" name = "file" />
+	<input type="submit" name= "submit" value="Upload video"/>
+
+</form>
+
+
+<?php\
+
+if(isset($_POST['submit']))
+{
+	echo "<br />".$name." has been uploades";
+}
+
+
+?>
+
+</body>
+
