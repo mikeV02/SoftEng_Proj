3f5ecc95af449a7443a7716616bb8450ac17c370
dacalder <dacalder@uark.edu>
2016-04-04 00:04:22 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -1,4 +1,29 @@
 
+<?php
+$db = "softeng";
+$password = "softeng";
+$username = "root";
+$db2 = "videos";
+$conn = mysqli_connect("localhost", $username, $password, $db);
+if(!$conn)
+{
+	die("Connection failed: ". mysqli_connect_error());
+}
+
+$sql = "SELECT url FROM videos";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "Url: " . $row["ulr"]"<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
 <!doctype html>
 <html>
 <head>
@@ -13,7 +38,7 @@
 
 <p>Find video</p>
 
-?>
+
 
 </body>
 </html>
