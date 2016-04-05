2f2f6e252c0be6a4ee09cfa850b41fbf61ab1d1d
dacalder <dacalder@uark.edu>
2016-04-04 23:41:56 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -1,3 +1,22 @@
+<?php
+$db = "softeng";
+$password = "softeng";
+$username = "root";
+$db2 = "videos";
+$con = mysqli_connect("localhost", $username, $password, $db) OR die('Could not connect to SQL:'. mysqli_connect_error());
+//display videos that are in database
+$sql = "SELECT * FROM videos ";
+$result = mysqli_query($con, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "url: " . $row['url']. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($con);
+?>
 
 <!doctype html>
 <html>
