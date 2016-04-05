4848671e5c2ef60de4ce40bb31890d3706d5dacb
dacalder <dacalder@uark.edu>
2016-04-04 23:55:33 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -17,6 +17,11 @@ if (mysqli_num_rows($result) > 0) {
 } else {
     echo "0 results";
 }
+<video width="320" height="240" controls>
+  <source src="videos/tst2.mp4" type="video/mp4">
+
+  Your browser does not support the video tag.
+</video>
 mysqli_close($con);
 }
 ?>
