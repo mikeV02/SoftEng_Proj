e9d4dac3d0e3ed25c8b5ad096b712e523b10d636
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-04 19:35:40 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -13,26 +13,12 @@
 	<body style="background-color:lightgrey"  columns-2>	
 
 
-	<div  ><video width="600" style="float: left; margin-bottom:30px;margin-right:30px;" controls>
+	<div id="videoTag">
+		<video width="600" style="float: left; margin-bottom:30px;margin-right:30px;" controls>
 		<source src="rabbit.mp4" type="video/mp4">
 		Your browser does not support HTML5 video.
-		</video></div>
-		
-	<div >
-	<div  ><video width="600" style="float: left; margin-bottom:30px;margin-right:30px;" controls>
-		<source src="rabbit.mp4" type="video/mp4">
-		Your browser does not support HTML5 video.
-		</video></div>
-		
-	<div >
-	<div >	<video width="600" style="float: left; margin-bottom:30px;margin-right:30px;"  controls>
-		  <source src="rabbit.mp4" type="video/mp4">
-		  Your browser does not support HTML5 video.
-		</video></div>
-	<div  >	<video width="600"style="float: left; margin-bottom:30px;margin-right:30px;" controls>
-		  <source src="rabbit.mp4" type="video/mp4">
-		  Your browser does not support HTML5 video.
-		</video></div>
+		</video>
+	</div>
 	
 	
 	
