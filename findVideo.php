d42c6543c47931985f8dbcc0bb6b01fcd378a385
dacalder <dacalder@uark.edu>
2016-04-05 22:15:18 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -1,47 +1,16 @@
-<?php
-if (isset($_POST['videoToFind'])) 
-{
-$db = "softeng";
-$password = "softeng";
-$username = "root";
-$db2 = "videos";
-$con = mysqli_connect("localhost", $username, $password, $db) OR die('Could not connect to SQL:'. mysqli_connect_error());
-//display videos that are in database
-$sql = "SELECT * FROM videos ";
-$result = mysqli_query($con, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        //echo "url: " . $row['url']. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-echo"<video width='320' height='240' controls>";
-echo"<source src='videos/tst2.mp4' type='video/mp4'>";
 
-echo " Your browser does not support the video tag.";
-echo "</video>";
-mysqli_close($con);
-}
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-
-<meta charset = "utf-8">
-<title> Find Video</title>
-</head>
-
-<h1 align="center"> Find Video  </h1>
-
 <body>
 
-<form action="findVideo.php" method="POST">
-  Video name: <input type="text" name="videoToFind"><br>
-  <input type="submit" value="Submit">
+<video width="320" height="240" controls>
+  <source src="videos/tst2.mp4" type="video/mp4">
+
+  Your browser does not support the video tag.
+</video>
 </form>
-   
+<form id="goBackMain" align="center" action="mainPage.php" method="POST">
+<p> Go back to <input type="submit" value="MainPage"></p>  
+</form>
 </body>
 </html>
