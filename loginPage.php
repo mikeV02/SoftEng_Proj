f47d3f7e6e07a106163b3063c227a0d07bb88e82
jaewung <no1ryu@hanmail.net>
2016-04-08 18:44:32 -0500

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -1,5 +1,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	<title>CSCE Group 15's project</title>
 
 	<script type="text/javascript">
