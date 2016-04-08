15338370bd1331a0779af0debe3eca5210b0880e
jaewung <no1ryu@hanmail.net>
2016-04-08 17:29:29 -0500

    Added files via upload

---
+++ aboutUs.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>about us</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2>Video Hog</h2>
+             
+  <table class="table member">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Major</th>
+		<th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Diego</td>
+        <td>Calderon</td>
+		<td>Computer Science</td>
+        <td>dacalder@uark.edu</td>
+      </tr>
+      <tr>
+        <td>Miguel</td>
+        <td>De Lora</td>
+		<td>Computer Engineer</td>
+        <td>medekora@uark.edu</td>
+      </tr>
+	  <tr>
+        <td>Fabian</td>
+        <td>Monasterio</td>
+		<td>Computer Science</td>
+        <td>frmonast@uark.edu</td>
+      </tr>
+	  <tr>
+        <td>Jaewung</td>
+        <td>Ryu</td>
+		<td>Computer Engineer</td>
+        <td>jryu@uark.edu</td>
+      </tr>
+	  <tr>
+        <td>Tuan</td>
+        <td>Truong</td>
+		<td>Computer Science</td>
+        <td>tbtruong@uark.edu</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
