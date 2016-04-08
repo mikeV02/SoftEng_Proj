1ccf095adeab67e5039202461e89d1ed80ca643f
jaewung <no1ryu@hanmail.net>
2016-04-08 15:25:00 -0500

    use bootstrap

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
 	</head>
 <h1>Welcome to CSCE Group 15's project page!</h1>
 	<body align="center " style="background-color:#172" >	
-	<div id="videoTag">
-		<img src="indexImage.png" alt="index picture" height="200" id="indexImg" width="1000" />
-		
 <html lang="en">
 <head>
   <title>Bootstrap Example</title>
@@ -54,23 +51,4 @@
 </body>
 </html>
 
-	<br>
-	</p>
-	<br>
-	<form align="center " id = "startGform" action="guestLogin.php" method="post" onsubmit="return guestC_Validation()" >
-	<p  id="pCreate">Please click <input type="submit" value="here">to login as a visitor</p>
-	</form>
-	<br>
-	<form id = "startCform"  align="center " action="newAccount.php" method="post" onsubmit="return guestC_Validation()" >
-	<p  id="pCreate">Please click <input type="submit" value="here"> to create a new account</p>
-	</form>
-	<br>
-	<form id = "startLform" align="center " action="loginPage.php" method="post" onsubmit="return guestL_Validation()" >
-	<p  id="pLogin">Please click <input type="submit" value="here">  to log in your account</p>
-	</form>
-	<br>
-	
-		<br>
-	</body>
-</html>
 <!-- 5ef4e0f21597916154413612f706fc7cee674bfd-->
