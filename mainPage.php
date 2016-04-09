e6a6e42435cb9fe8b17c27aa7a5a68959b792f80
jaewung <no1ryu@hanmail.net>
2016-04-09 17:46:51 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -16,7 +16,7 @@
 	<div class ="row">
 		<div class="col-sm-4">
 			<div id="videoTag">
-				<video alighn ="center" width="600" height="400" controls>
+				<video align ="center" width="600" height="400" controls>
 				<source src="rabbit.mp4" type="video/mp4">
 				Your browser does not support HTML5 video.
 				</video>
@@ -26,18 +26,18 @@
 			<form id = "startSform" align="center " action="uploadvideo.php" method="post" >
 			<p  id="sunmit">Please click <input type="submit" value="here"> to upload a video </p>
 			</form>
-			<br>
+			<br><br>
 			<form id = "startFform" align="center " action="findVideo.php" method="post" >
 			<p  id="sunmit">Please click <input type="submit" value="here"> to find a video </p>
-			</form>
+			</form><br><br>
 			<form id = "startWform" align="center " action="playVideo.html" method="post" onsubmit="return guestL_Validation()" >
 			<p  id="pLogin">Please click <input type="submit" value="here">  to watch a video</p>
 			</form>
-			<br>
+			<br><br>
    
 			<form id = "aboutUs" align="center " action="aboutUs.php" method="post" >
 			<input type="submit" value="about us"> 
-			</form>
+			</form><br><br>
 			
 		</div>
 		<div class="col-sm-4">
@@ -46,7 +46,7 @@
 			<a href="#">New videos <span class="badge">5</span></a><br>
 			<a href="#">Comments <span class="badge">10</span></a><br>
 			<a href="#">Updates <span class="badge">2</span></a>
-		</div>
+		</div><br><br>
 	</div><br><br>
 	<div class="row">
 	<form id = "logout" align="right "style="margin-right:50px;" action="index.php" method="post" >
