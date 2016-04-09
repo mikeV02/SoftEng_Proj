317f5fbfa595279bf44e20ec07a2c9d8c29a0444
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-08 20:18:31 -0500

    Update and rename inde.php to index.php

---
+++ videos/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+
+<meta charset = "utf-8">
+<title>Videos</title>
+</head>
+
+
+
+<body><!--
+<h1> Upload Video  </h1>
+<form action = "index.php" method="POST" enctype="multipart/form-data>
+	<input type="file" name = "file" />
+	<input type="submit" name= "submit" value="Upload video"/>
+
+</form> -->
+
+</body>
+
