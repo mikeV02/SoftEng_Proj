5accdf9c2512f1ddfd4fa94f67f75761d125c861
jaewung <no1ryu@hanmail.net>
2016-04-09 17:19:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
 	<body align="center " style="background-color:#172" >	
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
+   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
