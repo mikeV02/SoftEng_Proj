c1b0556216ebb495177cdd3955b27518e39ecc07
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-19 15:15:18 -0500

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -64,6 +64,7 @@ Released   : 20140107
 		$db_found = mysql_select_db($database, $db_handle);
 		
 		$username=$_POST["user_log"];
+		echo $username;
 		
 		$query=sprintf("SELECT ID, user_pass, user_email, display_name FROM users WHERE user_login = '%s'"
 				, mysql_real_escape_string($username));
