bb8a4478009451ebc44f24358f1f9af35cf9010e
frmonast <fabianm.riveros@gmail.com>
2016-04-19 00:49:36 -0500

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -1,6 +1,6 @@
-<!--
 <html xmlns="http://www.w3.org/1999/xhtml">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
@@ -9,12 +9,16 @@
 
 	<head>
 		<title>	Video Hog</title>
+		
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<link href="http://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet" />
+		<link href="home/default.css" rel="stylesheet" type="text/css" media="all" />
+		<link href="home/fonts.css" rel="stylesheet" type="text/css" media="all" />
+		
 		<link rel="stylesheet" href="style.css">
 	</head>
 
-
--->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--
 Design by TEMPLATED
 http://templated.co
@@ -24,18 +28,7 @@ Description: A two-column, fixed-width design with dark color scheme.
 Version    : 1.0
 Released   : 20140107
 -->
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<title>Video Hog</title>
-	<meta name="keywords" content="" />
-	<meta name="description" content="" />
-	<link href="http://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet" />
-	<link href="home/default.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="home/fonts.css" rel="stylesheet" type="text/css" media="all" />
-
-</head>
 <body>
 <div id="header" class="container">
 	<div id="logo">
