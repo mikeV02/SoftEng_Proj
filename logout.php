f6242b9247a1173be89eb2a9187184494de64d27
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-19 14:53:41 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,8 @@
 <?php
    session_start();
    unset($_SESSION["user"]);
+   $_SESSION['valid'] = false;
    
    echo 'You have cleaned session';
-   header('Refresh: 2; URL = ');
+   header('Refresh: 2; URL = http://softeng.mikedlv.com');
 ?>
