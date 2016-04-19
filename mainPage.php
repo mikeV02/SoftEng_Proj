ca3a83f846f60d058311111def38712171dc832f
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-19 14:29:33 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -62,7 +62,10 @@ session_start();
 									if ($result && mysql_num_rows($result) > 0)
 									{
 										$row=mysql_fetch_assoc($result);
-
+										$message = $row['fname'];
+										echo "<script type='text/javascript'>
+											alert('$message');
+											</script>";
 										$finame=$row['fname'];
 										$laname=$row['lname'];
 									}
