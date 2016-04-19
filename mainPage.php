78d6632a87c306bbd820822a0a9560abe0edccde
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-19 16:13:13 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -71,7 +71,7 @@ session_start();
 
 								echo "<h1 id='title'>$finame $laname</h1>";
 							?>
-							<p><a href="logout.php"><img>https://www.idldisplays.com/images/button-logout.png</img></a></p>
+							<p><a href="logout.php"><img src="https://www.idldisplays.com/images/button-logout.png"></a></p>
 						</div>
 
 					<!-- Nav -->
