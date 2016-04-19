59d21c16f61376497678e2da1c1e937ccd9995ee
Miguel <mikevelasquez02@users.noreply.github.com>
2016-04-19 15:10:25 -0500

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -5,23 +5,16 @@ session_start();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<!--
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
--->
-
-	<head>
-		<title>	Video Hog</title>
+<head>
+	<title>	Video Hog</title>
 		
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<link href="http://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet" />
-		<link href="home/default.css" rel="stylesheet" type="text/css" media="all" />
-		<link href="home/fonts.css" rel="stylesheet" type="text/css" media="all" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="http://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet" />
+	<link href="home/default.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="home/fonts.css" rel="stylesheet" type="text/css" media="all" />
 		
-		<link rel="stylesheet" href="style.css">   
+	<link rel="stylesheet" href="style.css">   
 	</head>
 
 <!--
@@ -75,7 +68,8 @@ Released   : 20140107
 		$query=sprintf("SELECT ID, user_pass, user_email, display_name FROM users WHERE user_login = '%s'"
 				, mysql_real_escape_string($username));
 
-		if ($db_found) {
+		if ($db_found)
+		{
 			$result = mysql_query($query);
 			if ($result && mysql_num_rows($result) > 0)
 			{
@@ -100,13 +94,14 @@ Released   : 20140107
 					</script>";
 				
 				}
-    			}
-			else
-    			{
-    				echo 'Username and Password NOT Found';
-    			}
+	    			else
+	    			{
+	    				echo 'Username or Password NOT Found';
+	    			}
+			}
 		}
-		else {
+		else
+		{
 			print "Database NOT Found.";
 			mysql_close($db_handle);
 		}
