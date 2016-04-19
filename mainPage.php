692de9feaedc89165d897865bc63b4071c0cde86
dacalder <dacalder@uark.edu>
2016-04-19 15:33:09 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -94,7 +94,6 @@ session_start();
 								<li><a href="#portfolio" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Videos</span></a></li>
 								<li><a href="broadcast.php" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Broadcast</span></a></li>
 								<li><a href="#about" id="about-link" class="skel-layers-ignoreHref"><span class="icon fa-user">Upload video</span></a></li>
-								<li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span class="icon fa-envelope">Contact</span></a></li>
 							</ul>
 						</nav>
 
@@ -188,8 +187,7 @@ session_start();
 				?>
 						</div>
 					</section>
-
-				<!-- Contact -->
+				<!-- Contact 
 					<section id="contact" class="four">
 						<div class="container">
 
@@ -214,7 +212,7 @@ session_start();
 						</div>
 					</section>
 
-			</div>
+			</div> -->
 
 		<!-- Footer -->
 			<div id="footer">
