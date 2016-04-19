3fd0ea2b67cf631e7ada4e1b8db96c0a2607db3b
dacalder <dacalder@uark.edu>
2016-04-19 00:36:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -55,26 +55,7 @@ Released   : 20140107
 			<li><a href="home/video.html" class="button">Find featured videos</a></li>
 		</ul>
 	</div>
-<div class="login">
-  <div class="heading">
-    <h2>Sign in</h2>
-    <form action="#">
 
-      <div class="input-group input-group-lg">
-        <span class="input-group-addon"><i class="fa fa-user"></i></span>
-        <input type="text" class="form-control" placeholder="Username or email">
-          </div>
-
-        <div class="input-group input-group-lg">
-          <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-          <input type="password" class="form-control" placeholder="Password">
-        </div>
-
-        <button type="submit" class="float">Login</button>
-       </form>
- 		</div>
- </div>
-</div>
 
 <!--
 <div id="page" class="container">
