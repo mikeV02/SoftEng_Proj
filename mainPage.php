7f8952c9f1a4514c35979a9f5a363b1965ca580b
jaewung <no1ryu@hanmail.net>
2016-04-29 13:17:11 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -93,6 +93,7 @@ session_start();
 								<li><a href="#top" id="top-link" class="skel-layers-ignoreHref"><span class="icon fa-home">Home</span></a></li>
 								<li><a href="#portfolio" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Videos</span></a></li>
 								<li><a href="#about" id="about-link" class="skel-layers-ignoreHref"><span class="icon fa-user">Upload video</span></a></li>
+								<li><a href="#search" id="search-link" class="skel-layers-ignoreHref"><span class="icon fa-user">Search video</span></a></li>
 								<li><a href="broadcast.php" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Broadcast</span></a></li>
 							</ul>
 						</nav>
