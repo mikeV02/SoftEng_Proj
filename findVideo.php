8b542bca29c817f1b1b1ff80a2174dc1ae1a31ac
dacalder <dacalder@uark.edu>
2016-04-30 18:28:12 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -88,22 +88,17 @@ if (isset($_POST['videoToFind'])) {
 	$vid_name = "%".$vid_name;
 	$sql = "SELECT * FROM videos WHERE name LIKE  '$vid_name' ";
 	$check = mysqli_query($con, $sql);
-	if($video = mysqli_fetch_array($check, MYSQLI_NUM)){;
-	$result = mysql_query($sql);
-	$rows = [];
-	while($row = mysqli_fetch_array($result))
-	{
-	    $rows[] = $row;
-	}
-	print_r($rows);
-}
+	while ($row = mysql_fetch_array($result))   
+	echo "yay";
+	if($video = mysqli_fetch_array($check, MYSQLI_NUM)){
 ?>
-	<div align="center">
+	
+	<!--<div align="center">
 	<video width="640" height="480" controls>
 	  <source src='<?php echo $vid_name ?>' type="video/mp4" align="center">
 	Your browser does not support the video tag.
 	</video>
-	</div>
+	</div>-->
 <?php
 	} else {
 		echo "0 results";
