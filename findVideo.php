db409a2c674c751926aaef5665cf0f63090f5722
dacalder <dacalder@uark.edu>
2016-05-01 17:39:52 -0500

    Update findVideo.php

--- findVideo.php
+++ findVideo.php
@@ -90,7 +90,9 @@ if (isset($_POST['videoToFind'])) {
 	$check = mysqli_query($con, $sql);
 	while($video = mysqli_fetch_array($check, MYSQLI_BOTH))
 	{
-		echo $video["name"];		
+		echo $video["name"];
+		$output = "videos/".$video["name"];
+		echo $output;
 	?>
 		<div align="center">
 		<video width="640" height="480" controls>
