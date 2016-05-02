f114a49f29916d1e5a8bf0046027e4dc061e549e
dacalder <dacalder@uark.edu>
2016-05-01 21:08:12 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -138,15 +138,6 @@ session_start();
 						<link href="http://vjs.zencdn.net/5.9.2/video-js.css" rel="stylesheet">
 							
 							  <!-- If you'd like to support IE8 -->
-						<script src="//mywebsite.com/jwplayer/jwplayer.js"></script>
-						<script>jwplayer.key="ABCdeFG123456SeVenABCdeFG123456SeVen==;</script>
-						<div id="myElement"></div>
-						<script type="text/JavaScript">
-						var playerInstance = jwplayer("myElement");
-						playerInstance.setup({
-						    file: "//content.jwplatform.com/videos/MEDIAID-TEMPLATEID.mp4"
-						});
-						</script>
 						 <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 							
 							  <center><video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
