ccbdf857a42c1a259bd4e6e6bfbaa6cf7bce2f2f
dacalder <dacalder@uark.edu>
2016-05-01 21:02:12 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -138,6 +138,14 @@ session_start();
 						<link href="http://vjs.zencdn.net/5.9.2/video-js.css" rel="stylesheet">
 							
 							  <!-- If you'd like to support IE8 -->
+						<script src="//content.jwplatform.com/libraries/PLAYERID.js"> </script>
+						<div id="myElement"></div>
+						<script type="text/JavaScript">
+						var playerInstance = jwplayer("myElement");
+						playerInstance.setup({
+						    file: "videos/rabbit.mp4"
+						});
+						</script>
 						 <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 							
 							  <center><video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
