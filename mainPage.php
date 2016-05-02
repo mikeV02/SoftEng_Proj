b74286de58f274b4f831cfafab2cb5aefeb234d6
dacalder <dacalder@uark.edu>
2016-05-01 22:42:30 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -151,6 +151,27 @@ session_start();
 							  </video></center>
 							
 							  <script src="http://vjs.zencdn.net/5.9.2/video.js"></script>
+							  
+							  	<!-- Load Facebook SDK for JavaScript -->
+									<div id="fb-root"></div>
+									<script>(function(d, s, id) {
+									  var js, fjs = d.getElementsByTagName(s)[0];
+									  if (d.getElementById(id)) return;
+									  js = d.createElement(s); js.id = id;
+									  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+									  fjs.parentNode.insertBefore(js, fjs);
+									}(document, 'script', 'facebook-jssdk'));</script>
+								
+									<!-- Your send button code -->
+								  <div class="fb-video" data-href="videos/rabbit.mp4" data-width="500">
+								     <div class="fb-xfbml-parse-ignore">
+								        <blockquote cite="https://www.facebook.com/facebook/videos/10153231379946729/">
+								           <a href="https://www.facebook.com/facebook/videos/10153231379946729/">How to Share With Just Friends</a>
+								           <p>How to share with just friends.</p>
+								           Posted by <a href="https://www.facebook.com/facebook/">Facebook</a> on Freitag, 5. Dezember 2014
+								        </blockquote>
+								     </div>
+								  </div>
 				<!-- About Me -->
 					<section id="about" class="three">
 						<div class="container">
