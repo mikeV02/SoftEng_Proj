d6f6289c3510875e727d1d9cce33208cb2832025
dacalder <dacalder@uark.edu>
2016-05-01 22:47:40 -0500

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -152,26 +152,31 @@ session_start();
 							
 							  <script src="http://vjs.zencdn.net/5.9.2/video.js"></script>
 							  
-							  	<!-- Load Facebook SDK for JavaScript -->
-									<div id="fb-root"></div>
-									<script>(function(d, s, id) {
-									  var js, fjs = d.getElementsByTagName(s)[0];
-									  if (d.getElementById(id)) return;
-									  js = d.createElement(s); js.id = id;
-									  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
-									  fjs.parentNode.insertBefore(js, fjs);
-									}(document, 'script', 'facebook-jssdk'));</script>
-								
-									<!-- Your send button code -->
-								  <div class="fb-video" data-href="videos/rabbit.mp4" data-width="500">
-								     <div class="fb-xfbml-parse-ignore">
-								        <blockquote cite="https://www.facebook.com/facebook/videos/10153231379946729/">
-								           <a href="https://www.facebook.com/facebook/videos/10153231379946729/">How to Share With Just Friends</a>
-								           <p>How to share with just friends.</p>
-								           Posted by <a href="https://www.facebook.com/facebook/">Facebook</a> on Freitag, 5. Dezember 2014
-								        </blockquote>
-								     </div>
-								  </div>
+    <!-- You can use Open Graph tags to customize link previews.
+    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+	<meta property="og:url"           content="http://www.your-domain.com/your-page.html" />
+	<meta property="og:type"          content="website" />
+	<meta property="og:title"         content="Your Website Title" />
+	<meta property="og:description"   content="Your description" />
+	<meta property="og:image"         content="http://www.your-domain.com/path/image.jpg" />
+</head>
+<body>
+
+	<!-- Load Facebook SDK for JavaScript -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<!-- Your share button code -->
+	<div class="fb-share-button" 
+		data-href="http://www.your-domain.com/your-page.html" 
+		data-layout="button_count">
+	</div>
 				<!-- About Me -->
 					<section id="about" class="three">
 						<div class="container">
