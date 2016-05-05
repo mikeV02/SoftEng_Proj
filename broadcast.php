304b5023107da862c7beeaf51179c73b02a5c9bf
geektbt <truongbatuan@gmail.com>
2016-05-05 11:37:29 -0500

    Update broadcast.php

--- broadcast.php
+++ broadcast.php
@@ -4,13 +4,13 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Broadcasting using WebRTC</title>
+        <title>VideoHog Broadcasting</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styleBC.css">
         
         <style>
             audio, video {
